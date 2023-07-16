urllib3/urllib3

Add experimental support for HTTP/1.1, HTTP/2 and HTTP/3
Detach ourselves from the http.client package. This requires a separate package to be installed, namely `urllib3-ext-hface` for the Sans-IO interfaces.

docs/conf.py
@@ -124,4 +124,8 @@ nitpick_ignore = [
     ("py:class", "urllib3.contrib.socks._TYPE_SOCKS_OPTIONS"),
     ("py:class", "urllib3.util.timeout._TYPE_DEFAULT"),
     ("py:class", "BaseHTTPConnection"),
+    ("py:class", "connection._TYPE_SOCKET_OPTIONS"),
+    ("py:class", "urllib3.backend.httplib._PatchedHTTPConnection"),
+    ("py:class", "urllib3.backend._base.ProxyHttpLibResponse"),
+    ("py:class", "QuicPreemptiveCacheType"),
 ]

test/with_traefik/test_protolevel.py
@@ -1,0 +1,59 @@
+from __future__ import annotations
+
+import pytest
+
+from urllib3 import HTTPHeaderDict, HTTPSConnectionPool, request
+from urllib3.exceptions import ProtocolError
+from urllib3.util.request import SKIP_HEADER
+
+from . import TraefikTestCase
+
+
+class TestProtocolLevel(TraefikTestCase):
+    def test_forbid_request_without_authority(self) -> None:
+        with pytest.raises(
+            ProtocolError,
+            match="do not support emitting HTTP requests without the `Host` header",
+        ):
+            request(
+                "GET",
+                f"{self.https_url}/get",
+                headers={"Host": SKIP_HEADER},
+                retries=False,
+            )
+
+    @pytest.mark.parametrize(
+        "headers",
+        [
+            [(f"x-urllib3-{p}", str(p)) for p in range(8)],
+            [(f"x-urllib3-{p}", str(p)) for p in range(8)]
+            + [(f"x-urllib3-{p}", str(p)) for p in range(16)],
+            [("x-www-not-standard", "hello!world!")],
+        ],
+    )
+    def test_headers(self, headers: list[tuple[str, str]]) -> None:
+        dict_headers = dict(headers)
+
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_mkcert
+        ) as p:
+            resp = p.request(
+                "GET",
+                f"{self.https_url}/headers",
+                headers=dict_headers,
+                retries=False,
+            )
+
+            assert resp.status == 200
+
+            temoin = HTTPHeaderDict(dict_headers)
+            payload = resp.json()
+
+            seen = []
+
+            for key, value in payload["headers"].items():
+                if key in temoin:
+                    seen.append(key)
+                    assert temoin.get(key) in value
+
+            assert len(seen) == len(dict_headers.keys())

test/with_traefik/test_send_data.py
@@ -1,0 +1,105 @@
+from __future__ import annotations
+
+from base64 import b64decode
+from io import BytesIO
+
+import pytest
+
+from urllib3 import HTTPSConnectionPool
+
+from . import TraefikTestCase
+
+
+class TestPostBody(TraefikTestCase):
+    def test_overrule_unicode_content_length(self) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_mkcert
+        ) as p:
+            resp = p.request("POST", "/post", body="🚀", headers={"Content-Length": "1"})
+
+            assert resp.status == 200
+            assert "Content-Length" in resp.json()["headers"]
+            assert resp.json()["headers"]["Content-Length"][0] == "4"
+
+    @pytest.mark.parametrize(
+        "method",
+        [
+            "POST",
+            "PUT",
+            "PATCH",
+        ],
+    )
+    @pytest.mark.parametrize(
+        "body",
+        [
+            "This is a rocket 🚀!",
+            "This is a rocket 🚀!".encode(),
+            BytesIO(b"foo" * 100),
+            b"x" * 10,
+            BytesIO(b"x" * 64),
+            b"foo\r\n",  # meant to verify that function unpack_chunk() in method send() work in edge cases
+            BytesIO(b"foo\r\n"),
+        ],
+    )
+    def test_h2n3_data(self, method: str, body: bytes | str | BytesIO) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_mkcert
+        ) as p:
+            for i in range(3):
+                if isinstance(body, BytesIO):
+                    body.seek(0, 0)
+
+                resp = p.request(method, f"/{method.lower()}", body=body)
+
+                assert resp.status == 200
+                assert resp.version == (20 if i == 0 else 30)
+
+                print(resp.json()["data"])
+
+                payload_seen_by_server: bytes = b64decode(resp.json()["data"][37:])
+
+                if isinstance(body, str):
+                    assert payload_seen_by_server == body.encode(
+                        "utf-8"
+                    ), f"HTTP/{resp.version/10} POST body failure: str"
+                elif isinstance(body, bytes):
+                    assert (
+                        payload_seen_by_server == body
+                    ), f"HTTP/{resp.version/10} POST body failure: bytes"
+                else:
+                    body.seek(0, 0)
+                    assert (
+                        payload_seen_by_server == body.read()
+                    ), f"HTTP/{resp.version/10} POST body failure: BytesIO"
+
+    @pytest.mark.parametrize(
+        "method",
+        [
+            "POST",
+            "PUT",
+            "PATCH",
+        ],
+    )
+    @pytest.mark.parametrize(
+        "fields",
+        [
+            {"a": "c", "d": "f", "foo": "bar"},
+            {"bobaaz": "really confident"},
+            {"z": "", "o": "klm"},
+        ],
+    )
+    def test_h2n3_form_field(self, method: str, fields: dict[str, str]) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_mkcert
+        ) as p:
+            for i in range(2):
+                resp = p.request(method, f"/{method.lower()}", fields=fields)
+
+                assert resp.status == 200
+                assert resp.version == (20 if i == 0 else 30)
+
+                payload = resp.json()
+
+                for key in fields:
+                    assert key in payload["form"]
+                    assert fields[key] in payload["form"][key]

jawah/urllib3.future

docs/conf.py
@@ -124,4 +124,8 @@ nitpick_ignore = [
     ("py:class", "urllib3.contrib.socks._TYPE_SOCKS_OPTIONS"),
     ("py:class", "urllib3.util.timeout._TYPE_DEFAULT"),
     ("py:class", "BaseHTTPConnection"),
+    ("py:class", "connection._TYPE_SOCKET_OPTIONS"),
+    ("py:class", "urllib3.backend.httplib._PatchedHTTPConnection"),
+    ("py:class", "urllib3.backend._base.LowLevelResponse"),
+    ("py:class", "QuicPreemptiveCacheType"),
 ]

test/with_traefik/test_protolevel.py
@@ -1,0 +1,62 @@
+from __future__ import annotations
+
+import pytest
+
+from urllib3 import HTTPHeaderDict, HTTPSConnectionPool
+from urllib3.exceptions import ProtocolError
+from urllib3.util.request import SKIP_HEADER
+
+from . import TraefikTestCase
+
+
+class TestProtocolLevel(TraefikTestCase):
+    def test_forbid_request_without_authority(self) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_authority
+        ) as p:
+            with pytest.raises(
+                ProtocolError,
+                match="do not support emitting HTTP requests without the `Host` header",
+            ):
+                p.request(
+                    "GET",
+                    f"{self.https_url}/get",
+                    headers={"Host": SKIP_HEADER},
+                    retries=False,
+                )
+
+    @pytest.mark.parametrize(
+        "headers",
+        [
+            [(f"x-urllib3-{p}", str(p)) for p in range(8)],
+            [(f"x-urllib3-{p}", str(p)) for p in range(8)]
+            + [(f"x-urllib3-{p}", str(p)) for p in range(16)],
+            [("x-www-not-standard", "hello!world!")],
+        ],
+    )
+    def test_headers(self, headers: list[tuple[str, str]]) -> None:
+        dict_headers = dict(headers)
+
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_authority
+        ) as p:
+            resp = p.request(
+                "GET",
+                f"{self.https_url}/headers",
+                headers=dict_headers,
+                retries=False,
+            )
+
+            assert resp.status == 200
+
+            temoin = HTTPHeaderDict(dict_headers)
+            payload = resp.json()
+
+            seen = []
+
+            for key, value in payload["headers"].items():
+                if key in temoin:
+                    seen.append(key)
+                    assert temoin.get(key) in value
+
+            assert len(seen) == len(dict_headers.keys())

test/with_traefik/test_send_data.py
@@ -1,0 +1,105 @@
+from __future__ import annotations
+
+from base64 import b64decode
+from io import BytesIO
+
+import pytest
+
+from urllib3 import HTTPSConnectionPool
+
+from . import TraefikTestCase
+
+
+class TestPostBody(TraefikTestCase):
+    def test_overrule_unicode_content_length(self) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_authority
+        ) as p:
+            resp = p.request("POST", "/post", body="🚀", headers={"Content-Length": "1"})
+
+            assert resp.status == 200
+            assert "Content-Length" in resp.json()["headers"]
+            assert resp.json()["headers"]["Content-Length"][0] == "4"
+
+    @pytest.mark.parametrize(
+        "method",
+        [
+            "POST",
+            "PUT",
+            "PATCH",
+        ],
+    )
+    @pytest.mark.parametrize(
+        "body",
+        [
+            "This is a rocket 🚀!",
+            "This is a rocket 🚀!".encode(),
+            BytesIO(b"foo" * 100),
+            b"x" * 10,
+            BytesIO(b"x" * 64),
+            b"foo\r\n",  # meant to verify that function unpack_chunk() in method send() work in edge cases
+            BytesIO(b"foo\r\n"),
+        ],
+    )
+    def test_h2n3_data(self, method: str, body: bytes | str | BytesIO) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_authority
+        ) as p:
+            for i in range(3):
+                if isinstance(body, BytesIO):
+                    body.seek(0, 0)
+
+                resp = p.request(method, f"/{method.lower()}", body=body)
+
+                assert resp.status == 200
+                assert resp.version == (20 if i == 0 else 30)
+
+                print(resp.json()["data"])
+
+                payload_seen_by_server: bytes = b64decode(resp.json()["data"][37:])
+
+                if isinstance(body, str):
+                    assert payload_seen_by_server == body.encode(
+                        "utf-8"
+                    ), f"HTTP/{resp.version/10} POST body failure: str"
+                elif isinstance(body, bytes):
+                    assert (
+                        payload_seen_by_server == body
+                    ), f"HTTP/{resp.version/10} POST body failure: bytes"
+                else:
+                    body.seek(0, 0)
+                    assert (
+                        payload_seen_by_server == body.read()
+                    ), f"HTTP/{resp.version/10} POST body failure: BytesIO"
+
+    @pytest.mark.parametrize(
+        "method",
+        [
+            "POST",
+            "PUT",
+            "PATCH",
+        ],
+    )
+    @pytest.mark.parametrize(
+        "fields",
+        [
+            {"a": "c", "d": "f", "foo": "bar"},
+            {"bobaaz": "really confident"},
+            {"z": "", "o": "klm"},
+        ],
+    )
+    def test_h2n3_form_field(self, method: str, fields: dict[str, str]) -> None:
+        with HTTPSConnectionPool(
+            self.host, self.https_port, ca_certs=self.ca_authority
+        ) as p:
+            for i in range(2):
+                resp = p.request(method, f"/{method.lower()}", fields=fields)
+
+                assert resp.status == 200
+                assert resp.version == (20 if i == 0 else 30)
+
+                payload = resp.json()
+
+                for key in fields:
+                    assert key in payload["form"]
+                    assert fields[key] in payload["form"][key]