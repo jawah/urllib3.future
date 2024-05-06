fbe653c8f2e6a37f457abec0f00e9d969cd8daaf
TAHRI Ahmed R <Ousret@users.noreply.github.com>
2024-05-06 06:05:41 +0200

    :bug: Fix usage of custom ssl context in conjunction of HTTP/3 (#114)
    Passing a ssl context containing manually loaded root certificates no
    longer is ignored with HTTP/3 over QUIC.
    Close #113

--- src/urllib3/backend/_async/hface.py
+++ src/urllib3/backend/_async/hface.py
@@ -215,6 +215,14 @@ class AsyncHfaceBackend(AsyncBaseBackend):
             if ssl_context.verify_mode == ssl.CERT_NONE:
                 allow_insecure = True
 
+            if ca_certs is None and ca_cert_dir is None and ca_cert_data is None:
+                ctx_root_certificates = ssl_context.get_ca_certs(True)
+
+                if ctx_root_certificates:
+                    ca_cert_data = "\n".join(
+                        ssl.DER_cert_to_PEM_cert(cert) for cert in ctx_root_certificates
+                    )
+
         if not allow_insecure and resolve_cert_reqs(cert_reqs) == ssl.CERT_NONE:
             allow_insecure = True
 
