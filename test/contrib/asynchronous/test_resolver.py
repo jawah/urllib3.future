1127a17cf83aeb786a2af36df86bc7a2c31d7c07
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2024-06-15 22:05:19 +0100

    :sparkle: Improve http.client deep compatibility offered by us
    also add "ARM64" in arch auto grabbing for qh3 installation

--- test/contrib/asynchronous/test_resolver.py
+++ test/contrib/asynchronous/test_resolver.py
@@ -148,7 +148,7 @@ async def test_1_1_1_1_ipv4_resolution_across_protocols(dns_url: str) -> None:
         quic_upgrade_via_dns_rr=False,
     )
 
-    assert any([_[-1][0] == "1.1.1.1" for _ in res])
+    assert any([_[-1][0] in ["1.1.1.1", "1.0.0.1"] for _ in res])
     await resolver.close()
 
 
