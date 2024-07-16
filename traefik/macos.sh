ade5ef53589aa4d83fd3a3daf625badd825c255c
Ahmed TAHRI <ahmed.tahri@cloudnursery.dev>
2024-07-17 00:15:30 +0100

    :bookmark: Release 2.8.903
    - Added ``IS_PYOPENSSL`` constant that is exposed by upstream in ``urllib3.util.ssl_`` submodule.
    - Fixed missing exception (``ImportError``) when importing ``urllib3.contrib.pyopenssl`` when PyOpenSSL isn't present in environment.
    - Lowered and simplified testing requirements for HTTP/2, and HTTP/3.
    - Added ``boto3``, ``sphinx``, and ``requests`` to our downstream test cases (nox).

--- traefik/macos.sh
+++ traefik/macos.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # MacOS in GitHub Actions does not ship with Docker due to legal reasons
 # we have to circumvent that limitation by using Colima as a viable alternative.
+set -e
 sudo security authorizationdb write com.apple.trust-settings.admin allow
 brew install docker
 brew install docker-compose
