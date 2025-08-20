f19601640bf14b732c2a7ed5495480f0c899a670
Ahmed TAHRI <tahri.ahmed@proton.me>
2025-08-20 01:23:22 +0100

    :heavy_check_mark: fix test_create_urllib3_context_default_ciphers

--- test/test_ssl.py
+++ test/test_ssl.py
@@ -131,14 +131,20 @@ class TestSSL:
     def test_create_urllib3_context_default_ciphers(
         self, monkeypatch: pytest.MonkeyPatch
     ) -> None:
+        support_min_max = hasattr(ssl_.SSLContext, "minimum_version")
+
         context = mock.create_autospec(ssl_.SSLContext)
         context.set_ciphers = mock.Mock()
         context.options = 0
+
         monkeypatch.setattr(ssl_, "SSLContext", lambda *_, **__: context)
 
         ssl_.create_urllib3_context(caller_id=ssl_._KnownCaller.NIQUESTS)
 
-        context.set_ciphers.assert_called_once_with(MOZ_INTERMEDIATE_CIPHERS)
+        if support_min_max:
+            context.set_ciphers.assert_called_once_with(MOZ_INTERMEDIATE_CIPHERS)
+        else:
+            context.set_ciphers.assert_not_called()
 
     @pytest.mark.parametrize(
         "kwargs",
