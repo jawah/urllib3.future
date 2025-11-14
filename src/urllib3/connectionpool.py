jawah/urllib3.future

commit c48a1326c6f6ddc6e7fe68af8d5755693936ff34

:bug: improve rare case of conn surplus when thread count is way superior to maxsize
this patch further improve excellency around our pooling solution by guarantying that we never exceed maxsize, with harmonious sharing between thread.

src/urllib3/connectionpool.py
@@ -478,7 +478,9 @@ class HTTPConnectionPool(ConnectionPool, RequestMethods):
         if self.pool is None:
             raise ClosedPoolError(self, "Pool is closed")
 
-        with self.pool.locate_or_hold() as swapper:
+        with self.pool.locate_or_hold(
+            block=self.block, placeholder_set=True
+        ) as swapper:
             self.num_connections += 1
             log.debug(
                 "Starting new HTTP connection (%d): %s:%s",
@@ -2080,7 +2082,13 @@ class HTTPSConnectionPool(HTTPConnectionPool):
             actual_host = self.proxy.host
             actual_port = self.proxy.port
 
-        with self.pool.locate_or_hold(block=self.block) as swapper:
+        # the placeholder is already set earlier
+        # because when we call get() and it returns 'None'
+        # it automatically insert a Placeholder to avoid
+        # concurrent / racing 'believe a spot is free'.
+        with self.pool.locate_or_hold(
+            block=self.block, placeholder_set=True
+        ) as swapper:
             conn = None
 
             if self.happy_eyeballs:

src/urllib3/util/traffic_police.py
@@ -540,6 +540,11 @@ class TrafficPolice(typing.Generic[T]):
                 not self._container or self.bag_only_saturated
             ) and self.maxsize is not None:
                 if self.maxsize > len(self._registry):
+                    self.put(
+                        ItemPlaceholder(),  # type: ignore[arg-type]
+                        immediately_unavailable=True,
+                        block=block,
+                    )
                     return None
 
             if self._container:
@@ -602,11 +607,19 @@ class TrafficPolice(typing.Generic[T]):
                         f"No connection available within {timeout} second(s)"
                     )
 
+                # in a very tight scenario (e.g. maxsize=1 and threads>=2)
+                # a first thread can get 'None' and a reserved spot from get(...)
+                # and the second can register a signal waiting for a conn being inserted
+                # in container. but the first interaction with container will be
+                # hotswapping of the initial "spot reserve" that will trigger
+                # signal with conn_or_pool being None. we can safely go nested call
+                # here.
                 if signal.conn_or_pool is None:
-                    raise UnavailableTraffic(
-                        "The signal was awaken without conn_or_pool assignment. "
-                        "This can be a bug within urllib3-future. "
-                        "You may open a ticket at https://github.com/jawah/urllib3.future for support"
+                    return self.get(
+                        block=block,
+                        timeout=timeout,
+                        non_saturated_only=non_saturated_only,
+                        not_idle_only=not_idle_only,
                     )
 
                 return signal.conn_or_pool
@@ -669,6 +682,7 @@ class TrafficPolice(typing.Generic[T]):
         self,
         traffic_indicator: MappableTraffic | None = None,
         block: bool = False,
+        placeholder_set: bool = False,
     ) -> typing.Generator[typing.Callable[[T], None] | T]:
         """Reserve a spot into the TrafficPolice instance while you construct your conn_or_pool.
 
@@ -693,12 +707,13 @@ class TrafficPolice(typing.Generic[T]):
         if traffic_indicator is not None:
             traffic_indicators.append(traffic_indicator)
 
-        self.put(
-            ItemPlaceholder(),  # type: ignore[arg-type]
-            *traffic_indicators,
-            immediately_unavailable=True,
-            block=block,
-        )
+        if not placeholder_set:
+            self.put(
+                ItemPlaceholder(),  # type: ignore[arg-type]
+                *traffic_indicators,
+                immediately_unavailable=True,
+                block=block,
+            )
 
         if traffic_indicator is not None:
             self._lock.release()

commit d7684c7d55dbc8d1944868e3947cee9e4eecef17

:bug: fix custom conn/pool integration
we accidentally broke docker with https://github.com/jawah/urllib3.future/pull/294/commits/c48a1326c6f6ddc6e7fe68af8d5755693936ff34

src/urllib3/connectionpool.py
@@ -711,7 +711,10 @@ class HTTPConnectionPool(ConnectionPool, RequestMethods):
             conn = self._new_conn()
             # as this branch is meant for people bypassing our main logic, we have to memorize the conn immediately
             # into our pool of conn.
-            self.pool.put(conn, immediately_unavailable=True)
+            with self.pool._lock:
+                if self.pool.busy_with_placeholder:
+                    self.pool.kill_cursor()
+                self.pool.put(conn, immediately_unavailable=True)
             return conn
 
     def _put_conn(self, conn: HTTPConnection) -> None:

src/urllib3/util/traffic_police.py
@@ -212,6 +212,15 @@ class TrafficPolice(typing.Generic[T]):
         with self._lock:
             return get_ident() in self._cursor
 
+    @property
+    def busy_with_placeholder(self) -> bool:
+        """Determine if the current thread hold a conn_or_pool that must be released asap."""
+        cursor_key = get_ident()
+        with self._lock:
+            return cursor_key in self._cursor and isinstance(
+                self._cursor[cursor_key].conn_or_pool, ItemPlaceholder
+            )
+
     def is_held(self, conn_or_pool: T) -> bool:
         with self._lock:
             cursor_key = get_ident()